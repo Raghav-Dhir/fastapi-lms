42f9aac0063c3ee78f16ccc6b9759edec5c17f14
Raghav Dhir <raghavdhir01.rd@gmail.com>
2023-02-08 17:40:02 +0530

    db and models

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from api import users, courses, sections
+from db.db_setup import engine
+from db.models import user, course
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="FastAPI LMS",
@@ -9,7 +15,7 @@ app = FastAPI(
         "name": "Raghav",
         "url": "https://github.com/Raghav-Dhir"
     }
-    )
+)
 
 app.include_router(users.router)
 app.include_router(sections.router)
